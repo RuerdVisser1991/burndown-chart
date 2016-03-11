a4c14bdd019f7adc80dfa4e1235b9a6875c36626
RuerdVisser1991 <ruerdvisser@live.nl>
2016-03-11 10:59:45 +0100

    Model aangemaakt met de 3 values

---
+++ db/migrate/20160311095930_create_days.rb
@@ -0,0 +1,11 @@
+class CreateDays < ActiveRecord::Migration
+  def change
+    create_table :days do |t|
+      t.date :date
+      t.integer :points
+      t.integer :sprint-id
+
+      t.timestamps null: false
+    end
+  end
+end
